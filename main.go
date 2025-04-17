efcd30327037d151836aab346eaa19b79547c305
David E. Wheeler <david.wheeler@tembo.io>
2025-04-17 17:09:46 -0400

    Add AWS upload

--- main.go
+++ main.go
@@ -7,9 +7,15 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 	"slices"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/service/s3/types"
 	"github.com/jackc/pgx/v5"
+	"github.com/walle/targz"
 )
 
 func h(err error) {
@@ -25,6 +31,8 @@ func main() {
 	dbs, err := list_databases(cfg)
 	h(err)
 	h(dump(cfg, dbs))
+	h(compress(cfg))
+	h(upload(cfg))
 }
 
 type Config struct {
@@ -34,11 +42,20 @@ type Config struct {
 	DBHost   string
 	DBUser   string
 	DBPass   string
+	Bucket   string
 	Plain    bool
 }
 
 func (c *Config) DumpDir() string {
 	return fmt.Sprintf("%v-%v-%v", c.OrgID, c.InstID, c.InstName)
+}
+
+func (c *Config) Tarball() string {
+	return c.DumpDir() + ".tar.gz"
+}
+
+func (c *Config) UploadURL() string {
+	return fmt.Sprintf("%v/%v", c.Bucket, c.Tarball())
 }
 
 func newConfig() *Config {
@@ -49,10 +66,13 @@ func newConfig() *Config {
 	flag.StringVar(&cfg.DBHost, "host", os.Getenv("PGHOST"), "Database host name")
 	flag.StringVar(&cfg.DBUser, "user", os.Getenv("PGUSER"), "Database username")
 	flag.StringVar(&cfg.DBPass, "pass", os.Getenv("PGPASSWORD"), "Database password")
+	flag.StringVar(&cfg.Bucket, "bucket", "", "S3 bucket name")
 	flag.BoolVar(&cfg.Plain, "text", false, "Plain text format")
 
 	flag.Parse()
-	if cfg.OrgID == "" || cfg.InstID == "" || cfg.InstName == "" || cfg.DBHost == "" || cfg.DBUser == "" || cfg.DBPass == "" {
+	if cfg.OrgID == "" || cfg.InstID == "" || cfg.InstName == "" ||
+		cfg.DBHost == "" || cfg.DBUser == "" || cfg.DBPass == "" ||
+		cfg.Bucket == "" {
 		usage()
 	}
 
@@ -60,86 +80,14 @@ func newConfig() *Config {
 }
 
 func usage() {
-	fmt.Printf("%v --org-id [ORG_ID] --inst-id [INST_ID] --inst-name [NAME] --conn [URI]\n", os.Args[0])
+	fmt.Printf(
+		"Usage:\n  %v --org-id [ORG_ID] --inst-id [INST_ID] --inst-name [NAME] --conn [URI] --bucket [S3_BUCKET]\n",
+		filepath.Base(os.Args[0]),
+	)
 	os.Exit(1)
 }
 
-func dump(cfg *Config, dbs []string) error {
-	dir := cfg.DumpDir()
-	if err := os.MkdirAll(dir, 0750); err != nil {
-		return err
-	}
-
-	type Job struct {
-		name string
-		cmd  *exec.Cmd
-	}
-
-	// Assemble the commands.
-	jobs := make([]Job, 0, len(dbs)+2)
-	jobs = append(jobs,
-		Job{
-			name: "roles",
-			cmd:  exec.Command("pg_dumpall", "-r", "-f", path.Join(dir, "roles.sql")),
-		},
-		Job{
-			name: "tablespaces",
-			cmd:  exec.Command("pg_dumpall", "-t", "-f", path.Join(dir, "tablespaces.sql")),
-		},
-	)
-
-	opts := []string{"-C", "-F"}
-	ext := ".sql"
-	if cfg.Plain {
-		opts = append(opts, "p")
-	} else {
-		opts = append(opts, "d", "-j", "8")
-		ext = ""
-	}
-	opts = append(opts, "-f")
-
-	for _, db := range dbs {
-		jobs = append(jobs,
-			Job{
-				name: db + " database",
-				cmd: exec.Command(
-					"pg_dump",
-					slices.Concat(opts, []string{path.Join(dir, "db-"+db+ext), db})...,
-				),
-			},
-		)
-	}
-
-	// Start the jobs.
-	for _, job := range jobs {
-		fmt.Printf("Dumping %v\n", job.name)
-		job.cmd.Env = append(job.cmd.Env,
-			"PGHOST="+cfg.DBHost,
-			"PGUSER="+cfg.DBUser,
-			"PGPASSWORD="+cfg.DBPass,
-			"PATH="+os.Getenv("PATH"),
-		)
-		job.cmd.Stderr = os.Stderr
-		job.cmd.Stdout = os.Stdout
-		if err := job.cmd.Start(); err != nil {
-			return err
-		}
-	}
-
-	// Wait for the commands to finish.
-	var ret error
-	for _, job := range jobs {
-		if err := job.cmd.Wait(); err != nil {
-			ret = err
-		}
-		fmt.Printf("Finished %v\n", job.name)
-	}
-
-	return ret
-}
-
 func list_databases(cfg *Config) ([]string, error) {
-
 	c, err := pgx.ParseConfig(fmt.Sprintf("user=%v password=%v host=%v", cfg.DBUser, cfg.DBPass, cfg.DBHost))
 	if err != nil {
 		return nil, err
@@ -160,4 +108,127 @@ func list_databases(cfg *Config) ([]string, error) {
 	}
 
 	return pgx.CollectRows(rows, pgx.RowTo[string])
+}
+
+func dump(cfg *Config, dbs []string) error {
+	dir := cfg.DumpDir()
+	if err := os.MkdirAll(dir, 0750); err != nil {
+		return err
+	}
+
+	type Job struct {
+		name string
+		cmd  *exec.Cmd
+		err  error
+	}
+
+	// Assemble the commands.
+	jobs := make([]Job, 0, len(dbs)+2)
+	jobs = append(jobs,
+		Job{
+			name: "roles",
+			cmd:  exec.Command("pg_dumpall", "-r", "-f", path.Join(dir, "roles.sql")),
+		},
+		Job{
+			name: "tablespaces",
+			cmd:  exec.Command("pg_dumpall", "-t", "-f", path.Join(dir, "tablespaces.sql")),
+		},
+	)
+
+	// Setup args according to configuration.
+	args := []string{"-C", "-F"}
+	ext := ""
+	if cfg.Plain {
+		args = append(args, "p")
+		ext = ".sql"
+	} else {
+		args = append(args, "d", "-j", "8")
+	}
+	args = append(args, "-f")
+
+	// Setup the jobs to dump each database.
+	for _, db := range dbs {
+		jobs = append(jobs,
+			Job{
+				name: db + " database",
+				cmd: exec.Command(
+					"pg_dump",
+					slices.Concat(args, []string{path.Join(dir, "db-"+db+ext), db})...,
+				),
+			},
+		)
+	}
+
+	// Start the jobs.
+	ch := make(chan Job)
+	for _, job := range jobs {
+		go func(job Job) {
+			// defer wg.Done()
+			fmt.Printf("Dumping %v\n", job.name)
+			job.cmd.Env = append(job.cmd.Env,
+				"PGHOST="+cfg.DBHost,
+				"PGUSER="+cfg.DBUser,
+				"PGPASSWORD="+cfg.DBPass,
+				"PATH="+os.Getenv("PATH"),
+			)
+			job.cmd.Stderr = os.Stderr
+			job.cmd.Stdout = os.Stdout
+			job.err = job.cmd.Run()
+			ch <- job
+		}(job)
+	}
+
+	// Wait for the commands to finish.
+	var ret error
+	for range len(jobs) {
+		job := <-ch
+		if job.err == nil {
+			fmt.Printf("Successfully dumped %v\n", job.name)
+		} else {
+			ret = job.err
+			fmt.Printf("Failed to dump dumped %v\n", job.name)
+		}
+	}
+
+	return ret
+}
+
+func compress(cfg *Config) error {
+	dir := cfg.DumpDir()
+	file := cfg.Tarball()
+	fmt.Printf("Archiving %v\n", file)
+	return targz.Compress(dir, file)
+}
+
+func upload(cfg *Config) error {
+	key := cfg.DumpDir()
+	file := cfg.Tarball()
+	fmt.Printf("Uploading %v...", file)
+
+	awsCfg, err := config.LoadDefaultConfig(context.TODO())
+	if err != nil {
+		return err
+	}
+	client := s3.NewFromConfig(awsCfg)
+	_ = client
+
+	fh, err := os.Open(file)
+	if err != nil {
+		fmt.Println("Failed")
+		return fmt.Errorf("cannot open %q: %v", file, err)
+	}
+	defer fh.Close()
+
+	if _, err := client.PutObject(context.Background(), &s3.PutObjectInput{
+		Bucket:            aws.String(cfg.Bucket),
+		Key:               aws.String(key),
+		Body:              fh,
+		ChecksumAlgorithm: types.ChecksumAlgorithmSha256,
+		ContentType:       aws.String("application/gzip"),
+	}); err != nil {
+		fmt.Println("Failed")
+		return err
+	}
+	fmt.Println("Done!")
+	return nil
 }
